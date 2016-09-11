1b8cecbe75b1445dc6464d049c29509ec49a5606
lex <atke@cs.helsinki.fi>
2016-09-11 19:33:30 +0300

    add database support

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
-@app.route("/")
-def hello():
-    return "kebab coming soon"
+@app.route('/')
+def kebab():
+    return 'kebab coming soon'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
