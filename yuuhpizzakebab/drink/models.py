375b98413ebeac457834503796bc520cafffd9c5
lex <atke@cs.helsinki.fi>
2016-09-24 18:55:42 +0300

    add crud for drinks

--- yuuhpizzakebab/drink/models.py
+++ yuuhpizzakebab/drink/models.py
@@ -1,6 +1,82 @@
+from sqlalchemy import text
+from yuuhpizzakebab import db
+
+def get_drinks():
+    result = db.engine.execute('select * from Drink')
+    drinks = []
+
+    for r in result:
+        drinks.append(Drink(r[0], r[1], r[2], r[3]))
+
+    return drinks
+
+
+def get_drink(id):
+    t = text('select * from Drink where id = :drink_id')
+    drink_results = db.engine.execute(t, drink_id=id)
+
+    if not drink_results:
+        return None
+
+    for r in drink_results:
+        return Drink(r[0], r[1], r[2], r[3])
+
+
+def delete_drink(id):
+    t = text('delete from Drink where id = :drink_id')
+    db.engine.execute(t, drink_id=id)
+
+
+def save_drink(drink):
+    t = text(
+        'insert into Drink (name, price, image_url) values (:name, :price, :image__url)')
+    db.engine.execute(t,
+                      name=drink.name,
+                      price=drink.price,
+                      image__url=drink.image_url)
+
+
+def update_drink(drink):
+    t = text(
+        'update Drink set name = :name, price = :price, image_url = :image__url where id = :drink_id')
+    db.engine.execute(t,
+                      name=drink.name,
+                      price=drink.price,
+                      image__url=drink.image_url,
+                      drink_id=drink.id)
+
 class Drink():
     def __init__(self, id, name, price, image_url):
         self.id = id
         self.name = name
         self.price = price
         self.image_url = image_url
+
+    def price_without_dollar_sign(self):
+        return str(self.price)[1:]
+
+    @staticmethod
+    def get_all():
+        return get_drinks()
+
+    @staticmethod
+    def get_by_id(id):
+        return get_drink(id)
+
+    def save(self):
+        if self.id:
+            update_drink(self)
+            return
+
+        save_drink(self)
+
+    @staticmethod
+    def delete_by_id(id):
+        delete_drink(id)
+
+    def delete(self):
+        if not self.id:
+            print 'can\'t delete without id t: drink'
+            return
+
+        delete_drink(self.id)
--- yuuhpizzakebab/drink/views.py
+++ yuuhpizzakebab/drink/views.py
@@ -1,10 +1,53 @@
-from yuuhpizzakebab import app
-from yuuhpizzakebab.drink.database_functions import get_drinks
-from flask import render_template
-
+from yuuhpizzakebab import app, admin_required, login_required
+from .models import Drink
+from flask import render_template, session, redirect, url_for, request
 
 @app.route('/drinks')
 def list_drinks():
-    return render_template('drinks.html',
+    return render_template('drink/drinks.html',
                            active_tab='drinks',
-                           drinks=get_drinks())
+                           drinks=Drink.get_all())
+
+@app.route('/drink/create', methods=['GET', 'POST'])
+@admin_required
+def create_drink():
+    if request.method == 'POST':
+        name = request.form['drink_name']
+        price = request.form['drink_price']
+        image_url = request.form['drink_image_url']
+
+        k = Drink(None, name, price, image_url)
+        k.save()
+
+        return redirect(url_for('list_drinks'))
+
+    return render_template('drink/create_drink.html', active_tab='drinks', )
+
+
+@app.route('/drink/edit/<int:drink_id>', methods=['GET', 'POST'])
+@admin_required
+def edit_drink(drink_id):
+    if request.method == 'POST':
+        name = request.form['drink_name']
+        price = request.form['drink_price']
+        image_url = request.form['drink_image_url']
+
+        k = Drink(drink_id, name, price, image_url)
+        k.save()
+
+        return redirect(url_for('list_drinks'))
+
+    drink = Drink.get_by_id(drink_id)
+
+    if not drink:
+        return redirect(url_for('list_drinks'))
+
+    return render_template('drink/edit_drink.html', active_tab='drinks', drink=drink)
+
+
+@app.route('/drink/delete/<int:drink_id>')
+@admin_required
+def delete_drink(drink_id):
+    Drink.delete_by_id(drink_id)
+
+    return redirect(url_for('list_drinks'))
