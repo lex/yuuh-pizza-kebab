64cca5cebc80754f23416ff3e2abdb7c524e35ee
lex <atke@cs.helsinki.fi>
2016-10-02 02:06:25 +0300

    tweak layout

--- yuuhpizzakebab/views.py
+++ yuuhpizzakebab/views.py
@@ -5,15 +5,3 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html', active_tab='home')
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html', active_tab='about')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html', active_tab='contact')
-
-
